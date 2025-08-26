778ac139510e5853d5a98fbaa87363816f71735c
Dembo, Antonio <antonio_dembo@dell.com>
2025-08-26 20:06:25 -0300

    Use service to get housing locations details data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { HousingLocation } from "../housing-location";
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -22,5 +23,10 @@ import { HousingLocation } from "../housing-location";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = []
+  housingLocationList: HousingLocation[] = [];
+  hosuingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.hosuingService.getAllHousingLocations();
+  }
 }
